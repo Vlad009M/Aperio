8410e6ba51dbd10c862bb43523f5fc689add9c38
Vlad009M <matovkavlad@gmail.com>
2026-05-21 17:26:27 +0300

    fix: remove selfDestroying PWA flag

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      selfDestroying: true,
       registerType: 'autoUpdate',
       includeAssets: ['Aperio.png', 'favicon.svg'],
       manifest: {
