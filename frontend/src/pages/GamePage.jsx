1cfe13a7ca7a88ec9a9332f71bc430ba8dc5f670
Vlad009M <matovkavlad@gmail.com>
2026-06-11 13:26:00 +0300

    fix: add useRef import to GamePage

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import toast from 'react-hot-toast'
 import api from '../api/index.js'
 import { useIsMobile } from '../hooks/useResponsive.js'
